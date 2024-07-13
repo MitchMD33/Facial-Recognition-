ef42919cc7b25c665246de4f3c33f0eb6f96df95
MitchMD33 <brown.l.mitchell@outlook.com>
2024-07-13 16:31:28 +0800

    Changed the urls to the deployed ones

--- facialrecognition/src/App.js
+++ facialrecognition/src/App.js
@@ -65,7 +65,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://facialrecognitionapi-8mbz.onrender.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -75,7 +75,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://facialrecognitionapi-8mbz.onrender.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- facialrecognition/src/Components/SignIn/SignIn.js
+++ facialrecognition/src/Components/SignIn/SignIn.js
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://facialrecognitionapi-8mbz.onrender.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
