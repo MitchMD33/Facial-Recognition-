0a4ecffe3df1ddc591d33a38246e2f66a59ac08b
MitchMD33 <brown.l.mitchell@outlook.com>
2024-07-14 09:08:46 +0800

    changing the colors around

--- facialrecognition/src/App.js
+++ facialrecognition/src/App.js
@@ -107,7 +107,7 @@ class App extends Component {
     const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
-         <ParticlesBg color="#ff0000" num={200} type="cobweb" bg={true} /> 
+         <ParticlesBg color="#FFFAF0" num={200} type="cobweb" bg={true} /> 
         <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
         { route === 'home'
           ? <div>
